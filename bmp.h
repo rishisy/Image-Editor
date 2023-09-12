06c891c3cb5ccb9abf512e527998be02161562ed
Rishi <rishicalifornia@gmail.com>
2023-09-12 19:09:31 +0530

    Update bmp.h

--- bmp.h
+++ bmp.h
@@ -6,6 +6,7 @@
 // Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
 // See https://en.wikipedia.org/wiki/C_data_types#stdint.h for more on stdint.h.
 
+
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
